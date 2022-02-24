c930e4bf811114389731b7e1e25c7187ee53f308
acalvom <andreac.3108@gmail.com>
2022-02-24 15:38:03 +0100

    Create Home component & add some mock cards with styles #1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import React from "react";
-import {Col, Container, Row} from "react-bootstrap";
+import React, {Fragment} from "react";
+import Home from "./components/Home";
 
 function App() {
     return (
-        <Container>
-            <Row>
-                <Col xs={12} sm={6} md={4}>
-                    <h2>Phone App. Mobile 1</h2>
-                </Col>
-                <Col xs={12} sm={6} md={4}>
-                    <h2>Phone App. Mobile 2</h2>
-                </Col>
-                <Col xs={12} sm={6} md={4}>
-                    <h2>Phone App. Mobile 3</h2>
-                </Col>
-            </Row>
-
-        </Container>
+        <Fragment>
+            <Home/>
+        </Fragment>
     );
 }
 
