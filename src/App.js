57caa0d7b6d1248ced789e51e4d0cb38bdad3614
acalvom <andreac.3108@gmail.com>
2022-02-24 14:50:39 +0100

    Install & Import Bootstrap 5.1.6 #1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
+import {Col, Container, Row} from "react-bootstrap";
 
 function App() {
-  return (
-    <div>
-      PHONE CATALOG APP
-    </div>
-  );
+    return (
+        <Container>
+            <Row>
+                <Col xs={12} sm={6} md={4}>
+                    <h2>Phone App. Mobile 1</h2>
+                </Col>
+                <Col xs={12} sm={6} md={4}>
+                    <h2>Phone App. Mobile 2</h2>
+                </Col>
+                <Col xs={12} sm={6} md={4}>
+                    <h2>Phone App. Mobile 3</h2>
+                </Col>
+            </Row>
+
+        </Container>
+    );
 }
 
 export default App;
