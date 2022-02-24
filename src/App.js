e99c285bb22e0f20694b0cdeaa95940897e0a3d9
acalvom <andreac.3108@gmail.com>
2022-02-24 21:14:11 +0100

    Install react-router-dom@5 #2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, {Fragment} from "react";
 import Home from "./components/Home";
+import PhoneDetail from "./components/PhoneDetail";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 function App() {
     return (
-        <Fragment>
-            <Home/>
-        </Fragment>
+        <Router>
+            <Fragment>
+                <Switch>
+                    <Route exact path="/"><Home/></Route>
+                    <Route path="/phone-detail"><PhoneDetail/></Route>
+                </Switch>
+            </Fragment>
+        </Router>
+
     );
 }
 
