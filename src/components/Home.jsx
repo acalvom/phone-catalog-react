2453ceabf1a78a599e20c04a048c1d6e35e55864
acalvom <andreac.3108@gmail.com>
2022-02-25 21:26:59 +0100

    Create "Add" button #8

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {Container} from "react-bootstrap";
+import {Button, Container} from "react-bootstrap";
 import axios from "axios";
 import Swal from "sweetalert2";
 import PhoneList from "./PhoneList";
@@ -35,6 +35,9 @@ const Home = () => {
     return (
         <Container>
             <h1 className="home-header mb-4">Phone Catalog</h1>
+            <div className="d-flex flex-wrap justify-content-end home-button-add">
+                <Button className="rounded-circle"><span className="fa-solid fa-plus"/></Button>
+            </div>
             {isFetching && <Loading/>}
             {!isFetching && phoneList && <PhoneList phoneList={phoneList}/>}
         </Container>
