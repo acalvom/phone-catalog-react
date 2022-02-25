0cdd1e65b939fccf92968aa0856677624efed9f4
acalvom <andreac.3108@gmail.com>
2022-02-25 15:28:41 +0100

    Add fetching data control & Improve API call with try/catch #5

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,11 +7,20 @@ import axios from "axios";
 const Home = () => {
 
     const [phoneList, setPhoneList] = useState([]);
+    const [isFetching, setIsFetching] = useState(true);
 
     useEffect(() => {
         const getPhoneList = async () => {
-            const response = await axios.get('/phones');
-            setPhoneList(response.data);
+            try {
+                setIsFetching(true);
+                const response = await axios.get('/phones');
+                setPhoneList(response.data);
+            } catch (e) {
+                console.log(e);
+            } finally {
+                setIsFetching(false);
+                console.log('End of request');
+            }
         }
         getPhoneList().then();
     }, []);
@@ -19,7 +28,7 @@ const Home = () => {
     return (
         <Container>
             <h1 className="home-header">Phone Catalog</h1>
-            {phoneList && <PhoneList phoneList={phoneList}/>}
+            {!isFetching && phoneList && <PhoneList phoneList={phoneList}/>}
         </Container>
     );
 };
