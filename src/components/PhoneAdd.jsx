126c01a3151d1479c55ba49fd6201cbbf4bf11d1
acalvom <andreac.3108@gmail.com>
2022-02-25 23:31:37 +0100

    Refactor code & Improve styles in the form #8

--- src/components/PhoneAdd.jsx
+++ src/components/PhoneAdd.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Col, Container, Row} from "react-bootstrap";
 import {useForm} from "react-hook-form";
+import './PhoneAdd.css';
 
 const PhoneAdd = () => {
     const {register, handleSubmit, formState: {errors}} = useForm();
@@ -8,74 +9,72 @@ const PhoneAdd = () => {
 
     return (
         <Container className="d-flex flex-wrap flex-column my-3 mx-auto align-content-center">
-            <h2 className="text-center">Add new phone information</h2>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <Row className="justify-content-between">
-                    <Col sm={10} md={4} className="mx-2">
-                        <div className="mb-3">
+            <h2 className="phone-add-title"><i className="fa-solid fa-mobile"/> Add new phone</h2>
+            <form onSubmit={handleSubmit(onSubmit)} className="phone-add-form">
+                <Row className="justify-content-between p-3">
+                    <Col sm={10} md={4}>
+                        <div className="phone-add-group">
                             <label htmlFor="phoneName" className="form-label">Phone name</label>
                             <input type="text" className="form-control form-control-sm" id="phoneName"
                                    {...register("phoneName", {required: true})}/>
-                            {errors.phoneName && "Phone name is required"}
+                            {errors.phoneName && <span>Phone name is required</span>}
                         </div>
-                        <div className="mb-3">
+                        <div className="phone-add-group">
                             <label htmlFor="phoneManufacturer" className="form-label">Manufacturer</label>
                             <input type="text" className="form-control form-control-sm" id="phoneManufacturer"
                                    {...register("phoneManufacturer", {required: true})}/>
-                            {errors.manufacturer && "Phone manufacturer is required"}
+                            {errors.manufacturer && <span>Manufacturer is required</span>}
                         </div>
-                        <div className="mb-3">
+                        <div className="phone-add-group">
                             <label htmlFor="phoneDescription" className="form-label">Description</label>
                             <textarea className="form-control form-control-sm" id="phoneDescription" rows="3"
                                       {...register("phoneDescription", {required: true})}/>
-                            {errors.phoneDescription && "Phone description is required"}
+                            {errors.phoneDescription && <span>Phone description is required</span>}
                         </div>
 
                     </Col>
-                    <Col sm={10} md={4} className="mx-2">
-                        <div className="mb-3">
+                    <Col sm={10} md={4}>
+                        <div className="phone-add-group">
                             <label htmlFor="phoneColor" className="form-label">Color</label>
                             <input type="text" className="form-control form-control-sm" id="phoneColor"
                                    {...register("phoneColor", {required: true})}/>
-                            {errors.phoneColor && "Phone color is required"}
+                            {errors.phoneColor && <span>Color is required</span>}
                         </div>
-                        <div className="mb-3">
-                            <label htmlFor="phonePrice" className="form-label">Price</label>
+                        <div className="phone-add-group">
+                            <label htmlFor="phonePrice" className="form-label">Price (€) </label>
                             <input type="text" className="form-control form-control-sm" id="phonePrice"
                                    {...register("phonePrice", {required: true})}/>
-                            {errors.phonePrice && "Phone price is required"}
+                            {errors.phonePrice && <span>Price is required</span>}
                         </div>
-                        <div className="mb-3">
+                        <div className="phone-add-group">
                             <label htmlFor="phoneScreen" className="form-label">Screen </label>
                             <input type="text" className="form-control form-control-sm" id="phoneScreen"
                                    {...register("phoneScreen")}/>
                         </div>
 
                     </Col>
-                    <Col sm={10} md={4} className="mx-2">
-                        <div className="mb-3">
+                    <Col sm={10} md={4}>
+                        <div className="phone-add-group">
                             <label htmlFor="phoneProcessor" className="form-label">Processor</label>
                             <input type="text" className="form-control form-control-sm" id="phoneProcessor"
                                    {...register("phoneProcessor")}/>
                         </div>
-                        <div className="mb-3">
+                        <div className="phone-add-group">
                             <label htmlFor="phoneRam" className="form-label">RAM</label>
                             <input type="text" className="form-control form-control-sm" id="phoneRam"
                                    {...register("phoneRam", {required: true})}/>
-                            {errors.phoneRam && "Phone RAM is required"}
+                            {errors.phoneRam && <span>RAM is required</span>}
                         </div>
-                        <div className="mb-3">
+                        <div className="phone-add-group">
                             <label htmlFor="phoneImage" className="form-label">Upload Image</label>
                             <input type="file" className="form-control form-control-sm" id="phoneImage"
                                    {...register("phoneImage", {required: true})}/>
-                            {errors.phoneImage && "Phone image is required"}
+                            {errors.phoneImage && <span>Image is required</span>}
                         </div>
                     </Col>
                 </Row>
-                <Row>
-                    <div className="mb-3">
-                        <button type="submit" className="btn btn-primary mb-3">Add phone</button>
-                    </div>
+                <Row className="d-flex justify-content-center my-2">
+                    <button type="submit" className="btn">Add phone</button>
                 </Row>
 
 
