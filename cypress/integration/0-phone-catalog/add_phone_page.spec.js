0f240cdb5f2f594df9f4434132f9ef4af82dc572
acalvom <andreac.3108@gmail.com>
2022-02-27 20:03:08 +0100

    Add more tests in "Add Phone" #12

--- cypress/integration/0-phone-catalog/add_phone_page.spec.js
+++ cypress/integration/0-phone-catalog/add_phone_page.spec.js
@@ -9,8 +9,25 @@ describe('Add new phone page', () => {
             .should('be.enabled')
     })
 
+    it('form has 9 fields to fill', () => {
+        cy.get('.phone-add-form input').should('have.length', 7)
+        cy.get('.phone-add-form textarea').should('have.length', 1)
+        cy.get('.phone-add-form select').should('have.length', 1)
+    })
+
     it('activates span validation when inputs are empty', () => {
         cy.get('button').click()
         cy.get('span').should('contain.text', 'is required')
+    })
+
+    it('fills eight fields', () => {
+        cy.get('.phone-add-form input#phoneName').type("Galaxy Note 2")
+        cy.get('.phone-add-form input#phoneColor').type("White")
+        cy.get('.phone-add-form input#phonePrice').type("258")
+        cy.get('.phone-add-form input#phoneRam').type("6")
+        cy.get('.phone-add-form input#phoneProcessor').type("Snapdragon X")
+        cy.get('.phone-add-form input#phoneScreen').type('8.2" Oled')
+        cy.get('.phone-add-form textarea#phoneDescription').type('Lorem ipsum dolor sit amet, consectetur adipiscing elit.')
+        cy.get('.phone-add-form select#phoneManufacturer').select("Samsung")
     })
 })
