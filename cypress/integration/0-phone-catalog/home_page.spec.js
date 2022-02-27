fd3ac62d9fb42104c50b1feeee9a1452160a3f13
acalvom <andreac.3108@gmail.com>
2022-02-27 19:07:34 +0100

    Add basic Home tests #12

--- cypress/integration/0-phone-catalog/home_page.spec.js
+++ cypress/integration/0-phone-catalog/home_page.spec.js
@@ -1,5 +1,10 @@
 describe('Phone Catalog Home Page', () => {
     it('loads successfully', () => {
         cy.visit('http://localhost:3000')
+
+        cy.get('h1').should('have.text', 'Phone Catalog')
+        cy.get('button')
+            .should('be.visible')
+            .should('be.enabled')
     })
 })
